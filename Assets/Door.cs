103caa2b1796df20b704bd76b4b4ebef9498bec8
james sullivan <james sullivan>
2017-02-19 23:22:20 -0500

    Added 2D Character Controller Script4

---
+++ Assets/Door.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    private Animator anim;
+    public bool doorTrigger;
+
+    void Start () {
+        anim = gameObject.GetComponent<Animator>();
+        doorTrigger = false;
+    }
+	
+	void Update () {
+        anim.SetBool("DoorTrigger", doorTrigger);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            doorTrigger = true;
+            Debug.Log("Door Opened");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            doorTrigger = false;
+            Debug.Log("Door Opened");
+        }
+    }
+}
---
+++ Assets/moveLeftRight.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveLeftRight : MonoBehaviour
+{
+
+    public float speed = 0.0f;
+    public string axisName = "Horizontal";
+    public Animator anim;
+    public bool idle = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        anim.SetFloat("Speed", (Input.GetAxis(axisName)));
+        anim.SetBool("Idle", idle);
+        Debug.Log(Input.GetAxis(axisName));
+        Debug.Log(idle);
+        if (Input.GetAxis(axisName) < 0)
+        {
+            idle = false;
+            //Hit collider left bool equals true.
+            //Hit collider right bool equals false.
+        }
+        else if (Input.GetAxis(axisName) > 0)
+        {
+            idle = false;
+            //Hit collider left bool equals false.
+            //Hit collider right bool equals true.
+        }
+        if (Input.GetAxis(axisName) == 0)
+        {
+            idle = true;
+        }
+        transform.position += transform.right * Input.GetAxis(axisName) * speed * Time.deltaTime;
+
+    }
+}
