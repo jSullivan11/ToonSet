14e1a09fa83e486ff54a536bd2dead0321166720
james sullivan <james sullivan>
2017-03-14 22:01:27 -0400

    Animated door

--- Assets/moveLeftRight.cs
+++ Assets/moveLeftRight.cs
@@ -1,44 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class moveLeftRight : MonoBehaviour
 {
 
+    private Rigidbody rb;
     public float speed = 0.0f;
-    public string axisName = "Horizontal";
-    public Animator anim;
-    public bool idle = true;
+    private string axisName = "Horizontal";
+    private Animator anim;
+    private bool idle = true;
 
     // Use this for initialization
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        anim.SetFloat("Speed", (Input.GetAxis(axisName)));
+        anim.SetFloat("Speed", (CrossPlatformInputManager.GetAxis(axisName)));
         anim.SetBool("Idle", idle);
-        Debug.Log(Input.GetAxis(axisName));
+        Debug.Log(CrossPlatformInputManager.GetAxis(axisName));
         Debug.Log(idle);
-        if (Input.GetAxis(axisName) < 0)
+        if (CrossPlatformInputManager.GetAxis(axisName) < 0)
         {
             idle = false;
             //Hit collider left bool equals true.
             //Hit collider right bool equals false.
         }
-        else if (Input.GetAxis(axisName) > 0)
+        else if (CrossPlatformInputManager.GetAxis(axisName) > 0)
         {
             idle = false;
             //Hit collider left bool equals false.
             //Hit collider right bool equals true.
         }
-        if (Input.GetAxis(axisName) == 0)
+        if (CrossPlatformInputManager.GetAxis(axisName) == 0)
         {
             idle = true;
         }
-        transform.position += transform.right * Input.GetAxis(axisName) * speed * Time.deltaTime;
-
+        //transform.position += transform.right * CrossPlatformInputManager.GetAxis(axisName) * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + transform.right * (CrossPlatformInputManager.GetAxis(axisName) * speed) * Time.deltaTime);
     }
 }
